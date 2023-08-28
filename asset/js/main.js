4bf7a66e82ddb5567136ff3e3c13e9bac138f9cb
Elisa Boscani <elisabosc90@gmail.com>
2023-08-28 15:14:47 +0200

    dice game

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,19 @@
 /* Generare un numero random da 1 a 6,
 sia per il giocatore sia per il computer.
  Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+//generare un numero per il giocatore Math.random
+//generare un numero per il pc Math.random
+// stabilire con if/else chi vince
+
+const player = Math.floor(Math.random() * 6) + 1;
+const pcPlayer = Math.floor(Math.random() * 6) + 1;
+console.log(player, pcPlayer);
+
+if (player > pcPlayer) {
+  console.log("hai vinto !!");
+} else if (player < pcPlayer) {
+  console.log("Hai perso!!");
+} else {
+  console.log("Avete pareggiato!!");
+}
